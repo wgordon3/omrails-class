fd87e17629ea7309c4c648ed4c0337fa0cb01262
wgordon3 <wgordon1@optonline.net>
2013-08-24 09:21:19 -0400

    Initial Commit - Bootstrap

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'jquery-rails'
+
+
+group :production do
+	gem 'pg'
+end
+
+group :development, :test do
+	gem 'sqlite3'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass', '~> 2.3.2.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '413b1cb9768eed9ff843e2acaae002d61c011503a2e0bca28e68f36438a3b8b3e27cc1282e35452a6d8d93d08f6e5e474cce8667af8f290ff4e2eaabd8f33a52'
